67e51c616fba49f21393b5f45355344cf00cc937
vivek-panchal <vivek.nsut@gmail.com>
2024-01-02 14:45:21 +0530

    component & Store configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
 
 function App() {
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <div>A blog app in appwrite</div>
-    </>
-  )
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
